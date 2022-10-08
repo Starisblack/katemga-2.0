a93b7727c8568ec3a3775061c99c7ad496b618b3
Scott Dialo <scottdialo@Scotts-MacBook-Pro.local>
2022-10-07 18:51:19 -0700

    added blog to nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -9,6 +9,7 @@ function Navigation() {
         <ul>
           <li>REAL ESTATE</li>
           <li>CARS/TRUCK</li>
+          <li>BLOG</li>
           <li>ELECTRONICS</li>
           <li>ABOUT US</li>
           <li>CONTACT US</li>
