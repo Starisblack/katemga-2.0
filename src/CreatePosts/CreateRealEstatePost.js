41f9bd1845e428d8ffcf6fb8d67c7c01298efc23
Scott Dialo <scottdialo@Scotts-MacBook-Pro.local>
2022-10-27 14:06:14 -0700

    database connected

--- src/CreatePosts/CreateRealEstatePost.js
+++ src/CreatePosts/CreateRealEstatePost.js
@@ -1,4 +1,28 @@
+import { useState } from "react";
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "../firebase-config";
+import { useNavigate } from "react-router-dom";
+
 function CreateRealEstatePost() {
+  const [title, setTitle] = useState("");
+  const [location, setLocation] = useState("");
+  const [price, setPrice] = useState("");
+  const [phone, setPhone] = useState("");
+  const [description, setDescription] = useState("");
+
+  const realEstatePostsCollectionRef = collection(db, "realEstatePosts");
+  let navigate = useNavigate();
+  const createRealEstatePost = async () => {
+    await addDoc(realEstatePostsCollectionRef, {
+      title,
+      location,
+      price,
+      phone,
+      description,
+    });
+    navigate("/homepage");
+  };
+
   return (
     <div className="createPostPage">
       <div className="cpContainer">
@@ -6,30 +30,55 @@ function CreateRealEstatePost() {
 
         <div className="inputGp">
           <label>Title</label>
-          <input type="text" />
+          <input
+            type="text"
+            onChange={(event) => {
+              setTitle(event.target.value);
+            }}
+          />
         </div>
 
         <div className="inputGp">
           <label>Location</label>
-          <input type="text" />
+          <input
+            type="text"
+            onChange={(event) => {
+              setLocation(event.target.value);
+            }}
+          />
         </div>
 
         <div className="inputGp">
           <label>Price</label>
-          <input type="number" />
+          <input
+            type="number"
+            onChange={(event) => {
+              setPrice(event.target.value);
+            }}
+          />
         </div>
 
         <div className="inputGp">
           <label>Phone Number</label>
-          <input type="number" />
+          <input
+            type="number"
+            onChange={(event) => {
+              setPhone(event.target.value);
+            }}
+          />
         </div>
 
         <div className="inputGp">
           <label>Description </label>
-          <textarea type="text"></textarea>
+          <textarea
+            type="text"
+            onChange={(event) => {
+              setDescription(event.target.value);
+            }}
+          ></textarea>
         </div>
 
-        <button class="" type="submit">
+        <button class="" type="submit" onClick={createRealEstatePost}>
           Post listing
         </button>
       </div>
