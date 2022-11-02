14c56c55f5eab489b737f67d17bd5eaefbed630a
Scott Dialo <scottdialo@Scotts-MacBook-Pro.local>
2022-11-01 22:22:35 -0700

    env file added

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -3,6 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import env from "react-dotenv";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -10,13 +11,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDfDD-V5YFcQyMvo4rgcnDHvw6PtoDp0SU",
-  authDomain: "katemga-dc780.firebaseapp.com",
-  projectId: "katemga-dc780",
-  storageBucket: "katemga-dc780.appspot.com",
-  messagingSenderId: "530300871492",
-  appId: "1:530300871492:web:296473755604e432bf6d11",
-  measurementId: "G-BCQJWGRZFW",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
