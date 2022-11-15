912a49611950ac753286f9294dd51f6e7c98b821
Scott Dialo <scottdialo@Scotts-MacBook-Pro.local>
2022-11-14 17:56:13 -0800

    cars listing rendered on car page

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
-const RealEstateModel = require("./models/RealEstateDB");
 
 app.use(express.json());
 app.use(cors());
@@ -12,34 +11,13 @@ mongoose.connect(process.env.MONGO_URI);
 const connection = mongoose.connection;
 
 const realEstateRouter = require("./routes/realEstate");
+const carsRouter = require("./routes/cars");
 
 app.use("/real-estate", realEstateRouter);
+app.use("/cars", carsRouter);
 
 connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
-});
-
-app.post("/createrealestatepost", async (req, res) => {
-  const title = req.body.title;
-  const location = req.body.location;
-  const price = req.body.price;
-  const phone = req.body.phone;
-  const description = req.body.description;
-
-  const realEstateData = new RealEstateModel({
-    title: title,
-    location: location,
-    price: price,
-    phone: phone,
-    description: description,
-  });
-
-  try {
-    await realEstateData.save();
-    res.send("Data have been added successfully!");
-  } catch (error) {
-    console.log(error);
-  }
 });
 
 const PORT = 3001;
